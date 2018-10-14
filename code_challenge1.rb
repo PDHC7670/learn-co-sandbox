7c2b6124cf19161891198c38c6361807582a6368
Porfirio D. Hernandez <porfiriohernandez483@gmail.com>
2018-10-14 20:04:51 +0000

    automatically backed up by learn

--- code_challenge1.rb
+++ code_challenge1.rb
@@ -1,3 +1,14 @@
 def badge_maker(array)
+    new_arr = []
   
+  array.each do |name|
+    new_arr << "My Name is #{name}"
+  end
+  
+return new_arr
+end
+
+my_array = ["Humzah", "Jordan", "Jessica"]
+
+puts my_method(my_array)
 end
